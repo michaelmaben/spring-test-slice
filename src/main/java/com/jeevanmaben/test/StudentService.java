83b561aac30497a5f4a0caa8f8a428511d5c51f7
Jeevan Maben <jeevanmaben@Jeevans-MacBook-Pro.local>
2021-03-06 09:59:42 +0530

    test caching functionality

--- src/main/java/com/jeevanmaben/test/StudentService.java
+++ src/main/java/com/jeevanmaben/test/StudentService.java
@@ -2,6 +2,7 @@ package com.jeevanmaben.test;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class StudentService {
     @Autowired
     StudentRepository studentRepository;
 
+    @Cacheable("students")
     public Student getStudentById(Long id) {
         return studentRepository.findById(id).orElse(null);
     }
---
+++ src/test/java/com/jeevanmaben/test/StudentCacheTest.java
@@ -0,0 +1,35 @@
+package com.jeevanmaben.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class StudentCacheTest {
+    @Autowired
+    private StudentService studentService;
+
+    @MockBean
+    private StudentRepository studentRepository;
+    @Test
+    public void testGetStudentUsingCache(){
+        //given
+        Long id = 123l;
+        given(studentRepository.findById(id)).willReturn(Optional.of(new Student(id, "Jeevan")));
+        //when
+        studentService.getStudentById(id);
+        studentService.getStudentById(id);
+        studentService.getStudentById(id);
+        studentService.getStudentById(id);
+        //then
+        then(studentRepository).should(times(1)).findById(id);
+    }
+}
